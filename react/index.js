0e16d3c48efedd207e33d50ea99ae74bcf97f121
abasb75 <abasbagheria@gmail.com>
2023-04-17 16:24:10 +0430

    0.1.0
    Signed-off-by: abasb75 <abasbagheria@gmail.com>

--- react/index.js
+++ react/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import { setupSazaStorageItems } from '../src';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -9,3 +10,7 @@ root.render(
 );
 
 
+// setup counter storage
+setupSazaStorageItems(
+    ['storageCounter'], // storageCounter will save on browser storage
+)
